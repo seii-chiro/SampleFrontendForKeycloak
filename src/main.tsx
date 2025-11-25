6d26366a595f08359ed3f9aec5af04ae571b7d56
Rhay from Workstation <javier.johnrhay@gmail.com>
2025-11-25 18:20:55 +0800

    wrap App component with KeycloakProvider for authentication context

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { KeycloakProvider } from './providers/KeycloakProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <KeycloakProvider>
+      <App />
+    </KeycloakProvider>
   </StrictMode>,
 )
