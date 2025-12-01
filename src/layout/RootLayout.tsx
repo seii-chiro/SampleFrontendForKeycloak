dc89a107408e3c0c5b70ff8b9c45a75315f9924e
Rhay from Workstation <javier.johnrhay@gmail.com>
2025-12-01 09:14:55 +0800

    remove unused Keycloak and user info hooks from RootLayout component

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,19 +1,9 @@
 import { Outlet } from "react-router"
-import { useKeycloak } from "../hooks/useKeycloak"
-import useUserInfo from "../hooks/useUserInfo";
 
 const RootLayout = () => {
-    const { keycloak } = useKeycloak();
-    const { userInfo } = useUserInfo();
 
-    return (
-        <>
-            <div>Root Layout</div>
-            <p>{keycloak?.token ?? "No Token"}</p>
-            <p>{userInfo ? JSON.stringify(userInfo) : "No User Info"}</p>
-            <Outlet />
-        </>
-    )
+    return <Outlet />
+
 }
 
 export default RootLayout
