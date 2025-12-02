6e453a013521dd99ddc3f2601ecaac4a685e8c2d
Rhay from Workstation <javier.johnrhay@gmail.com>
2025-12-02 14:05:57 +0800

    feat: wrap App component with QueryClientProvider for React Query integration

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { KeycloakProvider } from './providers/KeycloakProvider.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <KeycloakProvider>
-      <App />
-    </KeycloakProvider>
+    <QueryClientProvider client={queryClient}>
+      <KeycloakProvider>
+        <App />
+      </KeycloakProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
